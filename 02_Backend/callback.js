db395e4a89c963a3ea26642d85ab7200f1e2e9f3
JKyEun <j56237@naver.com>
2023-03-07 15:46:35 +0900

    20230307 백엔드 입문, callback 함수

---
+++ 02_Backend/callback.js
@@ -0,0 +1,16 @@
+// @ts-check
+
+function buySync(item, price, quantity, callback) {
+  console.log(`${item}을 ${quantity}개 골라서 점원에게 주었습니다.`);
+  setTimeout(() => {
+    const total = price * quantity;
+    console.log('점원이 계산을 완료했습니다.');
+    callback(total);
+  }, 1000);
+}
+
+function pay(total) {
+  console.log(`${total}원을 지불 하였습니다.`);
+}
+
+buySync('포켓몬빵', 1000, 5, pay);
