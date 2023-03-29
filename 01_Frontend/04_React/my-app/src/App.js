edd0896ba6ce40093d71bf1a9913288eabcefbda
JKyEun <j56237@naver.com>
2023-03-29 09:50:54 +0900

    .eslintrc, .prettierrc 세팅

--- 01_Frontend/04_React/my-app/src/App.js
+++ 01_Frontend/04_React/my-app/src/App.js
@@ -1,29 +1,7 @@
 import './App.css';
-import ChangeObj from './components/ChangeObj';
 
 function App() {
-  const pororoObjArr = [
-    {
-      name: '뽀로로',
-      age: '5',
-      nickName: '사고뭉치',
-    },
-    {
-      name: '루피',
-      age: '4',
-      nickName: '공주님',
-    },
-    {
-      name: '크롱이',
-      age: '5',
-      nickName: '장난꾸러기',
-    },
-  ];
-  return (
-    <div className='App'>
-      <ChangeObj objArr={pororoObjArr} />
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
